cb9bc2f9c589abb54b39644742ba06f8015f2419
selva-oritake <vpsuser@ik1-219-79869.vs.sakura.ne.jp>
2023-05-18 18:26:42 +0900

    いいね機能質問

--- php/thread_detail.php
+++ php/thread_detail.php
@@ -1,25 +1,51 @@
 <?php
   session_start();
   require_once('db_connect.php');
+
+
   
+  //スレッドのID一致するデータをthreadsから取得
   $stmt = $dbh->prepare('SELECT * FROM threads WHERE id=?');
   $stmt->execute(array($_GET['id']));
   $result = $stmt->fetch();
 
-  $stmt2 = $dbh->prepare('SELECT * FROM members WHERE id=?');
-  $stmt2->execute(array($result['member_id']));
-  $result2 = $stmt2->fetch();  
+  //会員IDが一致するデータをmembersから取得
+  $stmt = $dbh->prepare('SELECT * FROM members WHERE id=?');
+  $stmt->execute(array($result['member_id']));
+  $result2 = $stmt->fetch();  
 
-  $stmt3 = $dbh->prepare("SELECT * FROM comments JOIN members ON comments.member_id = members.id WHERE thread_id=?");
-  $stmt3->execute(array($result['id']));
-  $result3 = $stmt3->fetchAll();
+  //commentsとmembersを結合し、スレッドIDが一致するデータを取得
+  $stmt = $dbh->prepare("SELECT comments.*, members.name_sei, members.name_mei FROM comments LEFT JOIN members ON comments.member_id = members.id WHERE thread_id=?");
+  $stmt->execute(array($_GET['id']));
+  $result3 = $stmt->fetchAll();
+  
+  //上のを変更してみたパターン
+  //$stmt = $dbh->prepare("SELECT comments.*, name_sei, name_mei, count(comment_id) AS like_count FROM comments LEFT JOIN members ON comments.member_id = members.id LEFT JOIN likes ON comments.id = likes.comment_id WHERE thread_id=? GROUP BY comment_id");
+  //$stmt->execute(array($_GET['id']));
+  //$result3 = $stmt->fetchAll();
 
+  //コメント総数を取得
   $stmt = $dbh->prepare("SELECT COUNT(*) FROM comments WHERE thread_id=?");
   $stmt->execute(array($result['id']));
-  $count = $stmt->fetchColumn();  
+  $count = $stmt->fetchColumn();
+
+  //いいねの総数を取得 最初に試した方法
+  $stmt = $dbh->prepare("SELECT COUNT(comment_id) FROM likes WHERE comment_id=? GROUP BY comment_id");
+  $stmt->execute(array($row['id']));
+  $like_count = $stmt->fetchColumn();
+  
+  //ログイン者のいいね数を取得 1 or 0
+  $stmt = $dbh->prepare("SELECT COUNT(*) FROM likes WHERE comment_id=? AND member_id=? ");
+  $stmt->execute(array($result3['member_id']), $row['id']);
+  $my_like_count = $stmt->fetchColumn();
+
+  var_dump($like_count);//チェック用
+  var_dump($my_like_count);
+  var_dump($result3);
+
 
   if (!empty($_POST['comment_btn'])) {
-      if($_POST['comment'] === "" || mb_strlen($_POST['coment']) > 500) {
+      if($_POST['comment'] === "" || mb_strlen($_POST['comment']) > 500) {
         $error['comment'] = "blank";
       }
 
@@ -35,9 +61,24 @@
         $stmt->execute($params); 
 
         $_SESSION['comment'] = $_POST['comment'];
-        header('Location: ./');
+        header('Location: thread_detail.php?id='.$_GET['id'].'');
 
       }
+  }
+
+  //いいねボタン
+  if (!empty($_POST['heart_clear'])) {
+      $heart_clear = $_POST['heart_clear'];
+
+      $member_id = $_SESSION['member_id'];
+      $comment_id = $_POST['comment_id'];
+
+      $sql = "INSERT into likes (member_id, comment_id) values (:member_id, :comment_id)";
+      $stmt = $dbh->prepare($sql);
+      $params = array(':member_id' => $member_id, ':comment_id' => $comment_id);
+      $stmt->execute($params);
+
+      
   }
   
   //ページング
@@ -71,7 +112,7 @@
 
   <div class="title">
     <p><?php echo $result['title']; ?></p>
-    <p><?php echo $count. "コメント   ".$result['created_at']; ?></p>
+    <p><?php echo $count. "コメント"."  ".date("Y-m-d H:i", strtotime($result['created_at'])); ?></p>
   </div>
   
   <div class="prev_next">
@@ -80,24 +121,39 @@
   </div>
 
   <div class="detail">
-    <p><?php echo "投稿者:".$result2['name_sei']." ".$result2['name_mei']." ".$result['created_at']; ?></p>
+    <p><?php echo "投稿者:".$result2['name_sei']." ".$result2['name_mei']."  ".date("Y-m-d H:i", strtotime($result['created_at'])); ?></p>
     <p><?php echo nl2br($result['content']); ?></p>
   </div>
 
   <div class="comment_list">
-    <table>
-      <?php foreach ($comment_list as $row): ?>
+    <?php foreach ($comment_list as $row): ?>
+      <table>
         <tr>
           <td><?php echo $row['id']."."; ?></td>
           <td><?php echo $row['name_sei']; ?></td>
           <td><?php echo $row['name_mei']; ?></td>
-          <td><?php echo $row['created_at']; ?></td>
+          <td><?php echo date("Y-m-d H:i", strtotime($row['created_at'])); ?></td> 
         </tr>
-        <tr>
-          <td><?php echo nl2br($row['comment']); ?></td>
-        </tr>
-      <?php endforeach; ?>
-    </table>
+      </table>
+
+      <p><?php echo nl2br($row['comment']); ?></p>
+
+      
+      <?php if ($my_like_count < 1) : ?>
+        <form action="" method="post">
+          <input name="comment_id" type="hidden" value="<?php echo $row['id']; ?>">
+          <input name="heart_clear" class="heart_clear" type="submit" value="♡">
+          <span><?php echo $like_count; ?></span>
+        </form>
+      <?php else : ?>
+        <form action="" method="post">
+          <input name="comment_id" type="hidden" value="<?php echo $row['id']; ?>">
+          <input name="herat_red" class="heart red" type="submit" value="♥">
+          <span><?php echo $like_count[$result3['comment_id']]; ?></span>
+        </form>
+      <?php endif; ?> 
+      
+    <?php endforeach; ?>
   </div>
 
   <div class="prev_next">
